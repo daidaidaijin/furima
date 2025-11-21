1a4b416fce8dcf38cb2c2c72e9b3d5f21dc489ee
daijin <dadada_daijin@yahoo.co.jp>
2025-11-21 17:09:34 +0900

    second

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,13 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('name', 100)->nullable(false); //NOT NULL
+            $table->string('email', 255)->unique();       //UNIQUE
+            $table->string('password', 255)->nullable(false);
         });
+
+        //住所
+        $table->string('postal_code', 10)->nullable();
+        $table->string('address', 255)->nullable();
+        $table->string('address_detail', 255)->nullable();
+        $table->string('profile_image', 255)->nullable();
     }
 
     /**
---
+++ src/resources/views/product_list.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>商品一覧</title>
+  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
+</head>
+
+<body>
+  <header class="site-header">
+    <div class="header-inner">
+      <div class="header-logo"><div>
+
+      <nav class="header-nav">
+        <ul>
+          <li><a href="/logout">ログアウト</a></li>
+          <li><a href="/mypage">マイページ</a></li>
+          <li><a href="/sell">出品</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+
+</html>
